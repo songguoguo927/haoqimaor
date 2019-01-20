6c9c0dd16050d714c68539384ceec4261520b016
songguoguo927 <1351443467@qq.com>
2019-01-20 17:15:08 +0800

    删除功能

--- src/Btodolist.js
+++ src/Btodolist.js
@@ -17,13 +17,17 @@ class Btodolist extends Component{
                 value={this.state.inputValue}
                 onChange={this.handleInputChange.bind(this)}
             />
-            <button onClick={this.handleBtnClick.bind(this)}>add</button>
+            <button onClick={this.handleAddBtnClick.bind(this)}>add</button>
         </div>
         <ul>
            {
                this.state.list.map((item,index) =>{
-                return <li key={index}>{item}</li>
-              })
+                return (
+                    <div>
+                    <li key={index}>{item}</li>
+                   <button onClick={this.handleRemBtnClick.bind(this,index)}>remove</button>
+                   </div>)
+             })
           }
         </ul>
         </Fragment>
@@ -34,11 +38,20 @@ class Btodolist extends Component{
             inputValue:e.target.value
         })         
     }
-    handleBtnClick(){
+    handleAddBtnClick(){
         this.setState({
              list:[...this.state.list,this.state.inputValue],
              inputValue:''
              })     
+    }
+    handleRemBtnClick(index){
+        // console.log(index)//点击某一项按钮，展示对应下标
+        const updateList = [...this.state.list]//拷贝一个副本
+
+        updateList.splice(index,1)
+        this.setState({
+            list:updateList
+        })
     }
 }
 export default Btodolist
