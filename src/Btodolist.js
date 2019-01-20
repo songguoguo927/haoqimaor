a42618129a51390bdc03dbc3ff65501a4b19461f
songguoguo927 <1351443467@qq.com>
2019-01-20 16:58:04 +0800

    todolist增加功能

--- src/Btodolist.js
+++ src/Btodolist.js
@@ -5,7 +5,7 @@ class Btodolist extends Component{
         super(props)
         this.state = {
             inputValue:'',
-            list: []
+            list: ['学习react','学习node']
         }
     }
     render(){
@@ -17,29 +17,28 @@ class Btodolist extends Component{
                 value={this.state.inputValue}
                 onChange={this.handleInputChange.bind(this)}
             />
-            <button type="">add</button>
+            <button onClick={this.handleBtnClick.bind(this)}>add</button>
         </div>
         <ul>
-            <li>学习react</li>
-            <li>学习node</li>
+           {
+               this.state.list.map((item,index) =>{
+                return <li key={index}>{item}</li>
+              })
+          }
         </ul>
         </Fragment>
         )
     }
     handleInputChange(e){
-        //看e是啥，可在控制台打印一下 最终发现e.target.value的值是我们所需要的
-        // console.log(e.target.value) 
-        //在未显示绑定this是啥时，输入框值没有反应，打印出this为undefined，在18行显示绑定后再打印出this，为我们的Btodolist组件
-        // this.state.inputValue = e.target.value
-        //  console.log(this)
-        //  //显示绑定后，这个时候控制台可打印出正确的输入框的值this.state.inputValue
-        //  console.log(this.state.inputValue)
-         //再将更改后的值赋值给输入框，发现还是输入框值还是没反应。原来更改要用setState
-        //   this.state.inputValue = e.target.value
-         this.setState({
-             inputValue:e.target.value
-             }) 
-        
+        this.setState({
+            inputValue:e.target.value
+        })         
+    }
+    handleBtnClick(){
+        this.setState({
+             list:[...this.state.list,this.state.inputValue],
+             inputValue:''
+             })     
     }
 }
 export default Btodolist
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import App from "./App"
+// import App from "./App"
 import Btodolist from "./Btodolist"
 
 ReactDOM.render(<Btodolist />, document.getElementById("root"))
