31cb478358de698eac54591e62c73a4b3959bcb0
songguoguo927 <1351443467@qq.com>
2019-01-23 16:11:24 +0800

    上一步是创建Store，并从中获取数据渲染到列表中，下一步-改变store中的数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
 import reducer from './reducer'
 //下面可理解为创建一个管理员并把小本本给他
-const store = new createStore(reducer)
+const store = new createStore(
+    reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 
 export default store
